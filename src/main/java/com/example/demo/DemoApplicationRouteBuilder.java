ef03f8402a2c3eb361b4112eadeeb1a2b93fb145
rutvik-nvs <rutvik.panchal@nvsconsulting.io>
2022-01-19 03:36:13 -0600

    updated the Test String print

--- src/main/java/com/example/demo/DemoApplicationRouteBuilder.java
+++ src/main/java/com/example/demo/DemoApplicationRouteBuilder.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +14,16 @@ public class DemoApplicationRouteBuilder extends RouteBuilder {
 
         // Kafka Route for creating messages and pushing into the topic
         from("timer:time?period=5000")
-            .setBody(constant("Test"))
+            .process(new Processor(){
+                @Override
+                public void process(Exchange exchange) throws Exception {
+                    String output = System.getenv("TestString");
+                    if (output == null) {
+                        output = "Test";
+                    }
+                    exchange.getIn().setBody(output);
+                }
+            })
             .log(LoggingLevel.INFO, "CamelTest", "${body}")
             .to("mock:done");
     }
